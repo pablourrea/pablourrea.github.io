e14217bdef3a2e727f3fa0813b15c88e217ce9f6
Pablo Urrea <paurca@gmail.com>
2024-06-29 16:03:47 +0200

    Website published.

--- modules/contrib/highlightjs_input_filter/js/highlightjs_input_filter.js
+++ modules/contrib/highlightjs_input_filter/js/highlightjs_input_filter.js
@@ -2,25 +2,34 @@
  * @file
  * Enables syntax highlighting via highlight.js.
  */
-import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.8.0/es/core.min.js';
+import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/core.min.js';
 
 (function (Drupal, drupalSettings) {
   const languages = drupalSettings.highlightJsLanguages || [];
   const initEvent = new Event("highlightjs:init");
   const promises = Object.values(languages).map(language => {
-    return import(`https://unpkg.com/@highlightjs/cdn-assets@11.8.0/es/languages/${language}.min.js`)
-      .then(module => hljs.registerLanguage(language, module.default));
+    return import(`https://unpkg.com/@highlightjs/cdn-assets@11.9.0/es/languages/${language}.min.js`)
+      .then(module => hljs.registerLanguage(language, module.default))
+      .catch(e => console.error(`${e.name}: ${e.message}`));
   });
 
-  promises.push(
-    import('./highlightjs-copy.min.js')
-      .then(module => hljs.addPlugin(new module.default))
-) ;
+  if (drupalSettings.enableCopyButton == true) {
+    promises.push(
+        import('./highlightjs-copy.min.js')
+            .then(module => hljs.addPlugin(new module.default))
+    );
+  }
 
-  // Run this once, on first page load.
-  Promise.all(promises).then(() => {
-    hljs.highlightAll();
-    document.dispatchEvent(initEvent);
-  })
-
+  Drupal.behaviors.highlightInit = {
+    attach (context, settings) {
+      // Run this once, on first page load.
+      if (context !== document) {
+        return;
+      }
+      Promise.all(promises).then(() => {
+        hljs.highlightAll();
+        document.dispatchEvent(initEvent);
+      })
+    }
+  }
 })(Drupal, drupalSettings)
